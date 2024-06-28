1d8f4e88b0419ee53921cfb3b7c6991248bbb75d
kkademorais <kaike.carvalho@outlook.com>
2024-06-27 23:35:03 -0300

    Beginning of the List classes's exercises

---
+++ src/ListInterface/OperacoesBasicas/ListaTarefas.java
@@ -0,0 +1,24 @@
+package ListInterface.OperacoesBasicas;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ListaTarefas <Tarefa> {
+    private ArrayList<Tarefa> listaTarefas;
+    
+    public adicionarTarefa(String descricao){
+        return
+    }
+    public removerTarefa(String descricao){
+
+    }
+    public obterNumeroTotalTarefas(){
+
+    }
+    public obterDescricoesTarefas(){
+
+    }
+
+
+}
---
+++ src/ListInterface/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package ListInterface.OperacoesBasicas;
+
+public class Tarefa {
+    private String descricao;
+
+    //Construtor -> Setta a descricao
+    //Getter
+
+        //Construtor
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+}
