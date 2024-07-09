01a62dc5361dce5dc82baf999fe637e1bb85087f
kkademorais <kaike.carvalho@outlook.com>
2024-07-09 05:15:45 -0300

    Finished second Pesquisa List exercise

--- src/ListInterface/Pesquisa/SomaNumeros/Main.java
+++ src/ListInterface/Pesquisa/SomaNumeros/Main.java
@@ -1,7 +1,41 @@
 package ListInterface.Pesquisa.SomaNumeros;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        SomaNumeros ListaNumeros = new SomaNumeros();
+
+        System.out.println("Primeiramente verificando lista vazia: ");
+        System.out.println("Exibindo lista vazia: ");
+        ListaNumeros.exibirNumeros();
+
+        System.out.printf("\n\n");
+
+        System.out.println("Adicionando os números da sorte na lista");
+        ListaNumeros.adicionarNumero(22);
+        ListaNumeros.adicionarNumero(19);
+        ListaNumeros.adicionarNumero(5);
+        ListaNumeros.adicionarNumero(20);
+        ListaNumeros.adicionarNumero(27);
+
+        System.out.println("Exibindo lista premiada:");
+        ListaNumeros.exibirNumeros();
+        System.out.printf("\n\n");
+
+        System.out.println("Calculando a soma de todos esses números contidos na lista: ");
+        System.out.printf("A soma de todos os números da lista é igual a %d\n", ListaNumeros.calcularSoma());
+
+        System.out.println();
+
+        System.out.println("Encontrando maior valor dentro da lista: ");
+        System.out.printf("O maior valor dentre todos da lista é o %d\n", ListaNumeros.encontrarMaiorNumero());
+
+        System.out.println();
+
+        System.out.println("Encontrando menor valor dentro da lista: ");
+        System.out.printf("O menor valor dentre todos da lista é o %d\n", ListaNumeros.encontrarMenorNumero());
+
 
     }
 }
--- src/ListInterface/Pesquisa/SomaNumeros/SomaNumeros.java
+++ src/ListInterface/Pesquisa/SomaNumeros/SomaNumeros.java
@@ -2,38 +2,56 @@ package ListInterface.Pesquisa.SomaNumeros;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class SomaNumeros {
 
     //Atributo -> lista de números inteiros
-    public ArrayList<Numeros> ListaNumeros;
+    public ArrayList<Integer> ListaNumeros;
 
-    //Construtor -> cria lista vazia
-    public void ListaNumeros(){
+    public SomaNumeros(){
         this.ListaNumeros = new ArrayList<>();
     }
 
-    public void adicionarNumero(Numeros numeros){
-        //Numeros numeros = new Numeros();
+
+    //Métodos
+    public void adicionarNumero(int numeros){
         ListaNumeros.add(numeros);
     }
     public int calcularSoma(){
         int resultado = 0;
         for(int i = 0; i < ListaNumeros.size(); i++){
-            //resultado += ListaNumeros.get(i);
+            resultado += ListaNumeros.get(i);
         }
-
+        return resultado;
     }
     public int encontrarMaiorNumero(){
         int maiorValor = 0;
-        for(Numeros numeros: ListaNumeros){
-            //if(ListaNumeros.getFirst()){}
+        for(int i = 0; i < ListaNumeros.size(); i++){
+            if(i == 0){
+                maiorValor = ListaNumeros.get(i);
+            }
+            else if(ListaNumeros.get(i) > maiorValor){
+                maiorValor = ListaNumeros.get(i);
+            }
         }
+        return maiorValor;
     }
     public int encontrarMenorNumero(){
-
+        int menorValor = 0;
+        for(int i = 0; i < ListaNumeros.size(); i++){
+            if(i == 0){
+                menorValor = ListaNumeros.get(i);
+            }
+            else if(ListaNumeros.get(i) < menorValor){
+                menorValor = ListaNumeros.get(i);
+            }
+        }
+        return menorValor;
     }
     public void exibirNumeros(){
-
+        for(int numero: ListaNumeros){
+            System.out.printf("\n%d", numero);
+        }
     }
 
 
