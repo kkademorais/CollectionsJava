dd053c1c757bae5d619c5c4aa3689fccf72204db
kkademorais <kaike.carvalho@outlook.com>
2024-07-18 23:49:11 -0300

    Finished first OperacoesBasicas in set exercise

--- src/SetInterface/OperacoesBasicas/Convidados/ConjuntoConvidados.java
+++ src/SetInterface/OperacoesBasicas/Convidados/ConjuntoConvidados.java
@@ -1,6 +1,7 @@
 package SetInterface.OperacoesBasicas.Convidados;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class ConjuntoConvidados {
 
@@ -15,19 +16,38 @@ public class ConjuntoConvidados {
     //Métodos
 
         //Adiciona convidado no Set
-    public void adicionarConvidado(String nome, int codigoConvite){}
+    public void adicionarConvidado(String nome, int codigoConvite){
+        Convidado convidado = new Convidado(nome, codigoConvite);
+        SetConvidados.add(convidado);
+        System.out.printf("\nConvidado adicionado!");
+        System.out.printf("\nNome do convidado: %s", convidado.getNome());
+        System.out.printf("\nCódigo do convidado: %d\n", convidado.getCodigoConvite());
+    }
 
         //Busca convidado com código do convite e remove do Set
-    public void removerConvidadoPorCodigoConvite(int codigoConvite){}
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoRemover = null;
+        for(Convidado c: SetConvidados){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoRemover = c;
+            }
+        }
+        SetConvidados.remove(convidadoRemover);
+        System.out.printf("\nConvidado removido!");
+        System.out.printf("\nNome do convidado: %s", convidadoRemover.getNome());
+        System.out.printf("\nCódigo do convidado: %d\n", convidadoRemover.getCodigoConvite());
+    }
 
         //Retorna valor do número total de convidados contidos no Set
-    public int contarConvidados(){}
+    public void contarConvidados(){
+        System.out.printf("\nO número total de convidados confirmados é igual a %d \n", SetConvidados.size());
+    }
 
         //Getter -> mostra SetConvidados
     public void exibirConvidados(){
+        System.out.println("\nExibindo todos os convidados confirmados: ");
         for(Convidado c: SetConvidados){
-            System.out.println("Exibindo todos os convidados confirmados: ");
-            System.out.printf("\nNome do convidado: %s\n", c.getNome());
+            System.out.printf("\nNome do convidado: %s", c.getNome());
             System.out.printf("\nCódigo do convidado: %d\n", c.getCodigoConvite());
         }
     }
--- src/SetInterface/OperacoesBasicas/Convidados/Main.java
+++ src/SetInterface/OperacoesBasicas/Convidados/Main.java
@@ -3,5 +3,35 @@ package SetInterface.OperacoesBasicas.Convidados;
 public class Main {
     public static void main(String[] args) {
 
+        ConjuntoConvidados listaConvidados = new ConjuntoConvidados();
+
+        System.out.println("Exibindo lista de convidados inicialmente vazia!");
+        listaConvidados.exibirConvidados();
+
+        System.out.println();
+        System.out.println("Adicionando os convidados de honra!!");
+        listaConvidados.adicionarConvidado("Barreto", 20);
+        listaConvidados.adicionarConvidado("Vimieiro", 26);
+        listaConvidados.adicionarConvidado("Pistola", 15);
+        listaConvidados.adicionarConvidado("Piovani", 16);
+        listaConvidados.adicionarConvidado("Cubano", 8);
+
+        System.out.println();
+        System.out.println("Exibindo lista atual!");
+        listaConvidados.exibirConvidados();
+
+        System.out.println();
+        System.out.println("Convidado de código 8 não poderá comparecer. Favor removê-lo");
+        listaConvidados.removerConvidadoPorCodigoConvite(8);
+        System.out.println();
+
+        System.out.println("Exibindo lista atual!");
+        listaConvidados.exibirConvidados();
+
+        System.out.println();
+        System.out.println("Contar número de convidados na lista atualizada!");
+        listaConvidados.contarConvidados();
+        System.out.println();
+
     }
 }
